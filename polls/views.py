4e18f59b767d74d298a730622ac0d887dfe631e9
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-11-28 19:43:22 -0800

    Plural error

--- polls/views.py
+++ polls/views.py
@@ -22,11 +22,11 @@ def vote(request, question_id):
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html',  {
+        return render(request, 'polls/detail.html', {
             'question': p,
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.vote += 1
+        selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
